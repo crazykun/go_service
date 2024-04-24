5f5ba600870984c66b8c7636f67693e702d0b12e
jilikun <jilikun@gmail.com>
2024-04-24 10:07:49 +0800

    修改 重启脚本

--- restart.sh
+++ restart.sh
@@ -4,7 +4,7 @@ go mod tidy
 go build -o go_service
 chmod +x ./go_service
 echo "kill go_service service"
-killall go_service # kill go-admin service
+killall go_service # kill go_service service
 filename="./logs/output.log"
 nohup ./go_service >> $filename 2>&1 & #后台启动服务将日志写入output.log文件
 echo "run go_service success"
